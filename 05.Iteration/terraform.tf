553156c04f1aaca7e72507df8b49f4f60129377c
Arunkumar K Aruchamy <arunkumar-karuchamy@lab.marshub.io>
2021-03-11 15:35:00 +0530

    updated

--- 05.Iteration/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "remote-state"
-    storage_account_name = "udemylearnterraform"
-    container_name       = "tfstate"
-    key                  = "web.tfstate"
-  }
-}
