778a59b2f4282c41c5eedea6bc83e5acb01bb378
Arunkumar K Aruchamy <arunkumar-karuchamy@lab.marshub.io>
2021-03-15 22:43:19 +0530

    modules
    modules

--- 09/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "bastion_host_subnet_us2w" {
-  value = module.location_us2w.bastion_host_subnet
-}
-
-output "bastion_host_subnet_us2e" {
-  value = module.location_us2e.bastion_host_subnet
-}
